6133077d0fff4fb8d318dc660cff0bcf526eaeb5
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2019-05-13 22:05:02 -0400

    update

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "mamp",
+        password: "root",
+        database: "burgers_db"
+    });
+}
+
+connection.connect();
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+insert into burgers(burger_name, devoured)
+value('Cheese Burger', true),
+('Bison Burger', true),
+('Turkey Burger', false),
+('Buffalo Burger', false),
+('Black Bean Burger', true);
